f14971d435e538a3663cbff35e1f392dc57ff938
alyssaprog <60877425+alyssaprog@users.noreply.github.com>
2022-08-10 14:03:48 +0200

    Thingsboard + MQTT confirmation + RNG data
    Thingsboard part completed
    Add of a MQTT connection confirmation
    RNG data received = ACK + RNG for each of the 3 buoys
    New way to differentiate RNG data from different AUs on ThingsBoard
    Debug

--- src/Coast_board.ino
+++ src/Coast_board.ino
@@ -1,20 +1,35 @@
 /*
   COAST BOARD
   The coast can make 1 master communicate with multiple divers.
-  
-  Send the "$G..." message to the master buoy via LoRa every 8 sec, 
-  and receive the RNG calculated by the 3 buoys.
+
+  Send the "$G..." message to the master buoy via LoRa every 8 sec,
+  and receive the RNG calculated by the 3 buoys. Then send them to a
+  Thingsboard server.
 */
 
 // Include libraries
-#include <SPI.h>              
+#include <SPI.h>
 #include <LoRa.h>
+#include <WiFi.h>           // WiFi control for ESP32
+#include <ThingsBoard.h>    // ThingsBoard SDK
 #include "BufferedOutput.h"
 #include "SafeStringReader.h"
 
 
 // Initialize variables
-const long frequency = 866E6;   // LoRa Frequency 
+#define WIFI_AP_NAME       "IPhone de Alyssa"          // WiFi AP : has to be changed
+#define WIFI_PASSWORD      "alyssalyssa"               // WiFi password : has to be changed
+#define TOKEN              "9Ftr2eUbUVlcha97AdvB"      // device TOKEN
+#define THINGSBOARD_SERVER  "demo.thingsboard.io"      // ThingsBoard server instance
+
+// Initialize ThingsBoard client
+WiFiClient espClient;
+// Initialize ThingsBoard instance (setup with 256 bytes for JSON buffer)
+ThingsBoardSized<256> tb(espClient);
+// the Wifi radio's status
+int status = WL_IDLE_STATUS;
+
+const long frequency = 866E6;   // LoRa Frequency
 const int csPin = 5;            // LoRa radio chip select
 const int resetPin = 4;         // LoRa radio reset
 const int irqPin = 27;          // change for your board; must be a hardware interrupt pin
@@ -25,7 +40,7 @@ byte msgCount = 0;              // count of outgoing messages
 byte localAddress = 0xFF;       // address of this device
 byte destination = 0xAA;        // master buoy
 
-int interval = 8000;            // interval between each ping request (ms)
+int interval = 10000;            // interval between each ping request (ms)
 int i = 0;
 
 createBufferedOutput(input, 255, DROP_UNTIL_EMPTY);   // create an extra output buffer for the Serial2
@@ -33,11 +48,38 @@ createSafeStringReader(sfReader, 50, "\r\n");         // create SafeStringReader
 createSafeString(accoustSignal, 50);                  // SafeString to save the "$G..." message as long as it is the same
 
 
+// WiFi connection
+void InitWiFi()
+{
+  Serial.println("Connecting to AP ...");
+  // attempt to connect to WiFi network
+
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+  }
+}
+
 // Initialization of LoRa
-void LoRaInit(){
+void LoRaInit() {
   LoRa.setPins(csPin, resetPin, irqPin);
 
-  if (!LoRa.begin(frequency)) {           
+  if (!LoRa.begin(frequency)) {
     Serial.println("LoRa init failed. Check your connections.");
     ESP.restart();                     // if failed, restart ESP
   }
@@ -48,15 +90,18 @@ void LoRaInit(){
 
 // Send a message to master buoy
 void sendMessage(String& outgoing) {
-    LoRa.beginPacket();                   // start packet
-    LoRa.write(destination);              // add destination address
-    LoRa.write(localAddress);             // add sender address
-    LoRa.write(msgCount);                 // add message ID
-    LoRa.write(outgoing.length());        // add payload length
-    LoRa.print(outgoing);                 // add payload
-    LoRa.endPacket();                     // finish packet and send it
-    msgCount++;                           // increment message ID
-    Serial.println(outgoing);
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+
+  Serial.println();
+  Serial.print("\nSIGNAL SENT : "); Serial.println(outgoing);
+  Serial.print();
 }
 
 
@@ -75,9 +120,9 @@ void onReceive(int packetSize) {
   while (LoRa.available()) {
     incoming += (char)LoRa.read();
   }
-  
+
   // check length for error
-  if (incomingLength != incoming.length()) {  
+  if (incomingLength != incoming.length()) {
     Serial.println("error: message length does not match length");
     return;                             // skip rest of function
   }
@@ -89,8 +134,21 @@ void onReceive(int packetSize) {
 
   // if message is for this device, or broadcast, print details:
   Serial.println("Message ID: " + String(incomingMsgId));
-  Serial.println("Message: " + incoming);
+  Serial.println(incoming);
   Serial.println();
+
+  // Uploads new telemetry to ThingsBoard using MQTT
+  char* MQTT = strstr(incoming.c_str(), "MQTT");    // check if the message contains "MQTT" 
+  
+  if (!MQTT){                                       // if not, data is sent bc we don't want to send the MQTT confirmation message
+    Serial.print("Sending data to dashboard...");
+
+    char d[incoming.length() + 1];                  // convert string into char array
+    strcpy(d, incoming.c_str());
+  
+    tb.sendTelemetryString("Buoys RNG", d);         // process messages 
+    tb.loop();                                
+  }
 }
 
 
@@ -107,27 +165,65 @@ boolean runEvery(unsigned long interval) {
   return false;
 }
 
+
 // Put each diver ping command in a tab and recover the size of the tab
-int splitPings(){
+int splitPings() {
   int i = 0; // counts the number of diver to ping
-  
+
   // Convert String into char array
   char str[accoustSignal.length() + 1]; 
   strcpy(str, accoustSignal.c_str()); 
- 
+
   // Returns first token and save it in the outgoing message tab
   char *token = strtok(str, ";");
   outgoing[i] = token;
- 
+
   // Keep printing tokens while one of the
   // delimiters present in str[].
-  while (token != NULL){
-      token = strtok(NULL, ";");
-      i++;
-      outgoing[i] = token;        // save each token in the outgoing message tab
+  while (token != NULL) {
+    token = strtok(NULL, ";");
+    i++;
+    outgoing[i] = token;        // save each token in the outgoing message tab
   }
 
   return i;
+}
+
+// Remove space(s) from a string
+//char *removeSpaces(const char *str){
+//  int i = 0, j = 0;
+//  char* new_str;
+//  
+//  while (str[i] != '\0') {
+//    if (str[i] != ' '){
+//      new_str[j] = str[i];
+//      j++;
+//    }
+//    i++; 
+//  }
+//  
+//  new_str[j] = '\0';
+//  Serial.println("new_str");
+//  Serial.println(new_str);
+//  return new_str;
+//}
+
+// Reconnect the board to ThingsBoard, if needed
+void ThingsBoardConnection(){
+  if (!tb.connected()) {
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+
+    Serial.println("Succeed to connect");
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -138,6 +234,9 @@ void setup() {
   while (!Serial);
 
   Serial.println("COAST BOARD");
+
+  // WiFi connection
+  InitWiFi();
 
   // Initialize LoRa
   LoRaInit();
@@ -150,9 +249,18 @@ void setup() {
 
 
 void loop() {
+  // Reconnect to WiFi, if needed
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+    return;
+  }
+
+  // Reconnect to ThingsBoard, if needed
+  ThingsBoardConnection();
+
   // Save text written on Serial
-  while (Serial.available()){  
-    if(sfReader.read())
+  while (Serial.available()) {
+    if (sfReader.read())
       accoustSignal = sfReader;
   }
 
@@ -160,21 +268,24 @@ void loop() {
   int sizeOutgoing = splitPings();
 
   // Send a ping request to each diver one after the other, every time frame
-  if (runEvery(interval)){                // process every 5 seconds
-    if(accoustSignal.isEmpty() == 0) {    // if the message to send is not empty
-      if(i == sizeOutgoing)   
+  if (runEvery(interval)) {               // process every 5 seconds
+    if (accoustSignal.isEmpty() == 0) {   // if the message to send is not empty
+      if (i == sizeOutgoing)
         i = 0;                            // start again from beginning of tab when end is reached
-     
+
       sendMessage(outgoing[i]);           // send ping to the desired diver
-      Serial.println("\nSIGNAL SENT\n");
+      
+      char nameDiver[outgoing[i].length() + 1];         // convert string into char array
+      strcpy(nameDiver, outgoing[i].c_str());
+      tb.sendTelemetryString("Buoys RNG", nameDiver);   // send the "id" of the diver that has be pinged on Thingsboard
       
       i++;
     }
   }
-  
+
   // Receive the RNG tab from master buoy
   onReceive(LoRa.parsePacket());
 
   // Release one byte from the buffer each 9600 baud
-  input.nextByteOut();   
+  input.nextByteOut();
 }
